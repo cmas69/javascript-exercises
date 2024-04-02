36603cc9220facf9d3d6205ee5696548f2bb9f8f
Chris <119714898+cmas69@users.noreply.github.com>
2024-04-03 12:15:57 +1300

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const re = /[0-9A-Za-z]/;
 
+    let arr1 = string
+        .toLowerCase()
+        .split("")
+        .filter((v) => {
+            return v.match(re) != null;
+        });
+
+    let clean = arr1.join("");
+    let reversed = arr1.reverse().join("");
+    
+//    console.log(`clean: ${clean} == reversed: ${reversed}`);
+    return clean == reversed;
 };
 
 // Do not edit below this line
