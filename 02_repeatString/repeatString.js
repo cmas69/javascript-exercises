22381de30f5a8ec516b571c00cad533ca9701544
Chris <119714898+cmas69@users.noreply.github.com>
2024-04-02 16:38:23 +1300

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,6 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, count) {
+    return (count < 0) ? 'ERROR' : (count == 0) ? '' : string.repeat(count);
+}
 
 // Do not edit below this line
 module.exports = repeatString;
