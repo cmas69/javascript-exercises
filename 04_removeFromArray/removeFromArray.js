deb767c1af608e3b463c6cf4183aae3e4620bdc6
Chris <119714898+cmas69@users.noreply.github.com>
2024-04-02 20:22:30 +1300

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    console.log(array);
+    console.log(args);
 
+    if(args.length == 0) {
+        return array;
+    }
+
+    let value = args.shift();
+
+    console.log(`value: ${value}, remaining args: ${args}`);
+
+    return removeFromArray(array.filter((v, i, a) => {
+        return v !== value;
+    }), ...args);
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
