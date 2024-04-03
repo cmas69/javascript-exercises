1b7b9b6f30657eb7e4e83ef1b26e3474d004aa4c
Chris <119714898+cmas69@users.noreply.github.com>
2024-04-03 14:21:18 +1300

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const GOLDEN_RATIO = 1.618034
+const DIVISOR = Math.sqrt(5);
 
+const fibonacci = function(step) {
+    let actualStep = +step;
+
+    if(actualStep < 0) { return 'OOPS'; }
+    
+    return Math.round((Math.pow(GOLDEN_RATIO, actualStep) - Math.pow(1 - GOLDEN_RATIO, actualStep)) / DIVISOR);
 };
 
 // Do not edit below this line
