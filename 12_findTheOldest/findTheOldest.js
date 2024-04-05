fa5921c343e687a023a18433200bb73ece6ce413
Chris <119714898+cmas69@users.noreply.github.com>
2024-04-05 17:01:10 +1300

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(peeps) {
+    let winner = peeps
+        .map((v, i, a) => {
+            if(!("yearOfBirth" in v)) { v.yearOfBirth = 2024; }
+            if(!("yearOfDeath" in v)) { v.yearOfDeath = 2024; }
 
+            return v;
+        })
+        .reduce((oldest, next) => {
+            let p1 = Number(oldest.yearOfDeath) - Number(oldest.yearOfBirth);
+            let p2 = Number(next.yearOfDeath) - Number(next.yearOfBirth);
+
+            return p1 > p2 ? oldest : next;
+    });
+
+    return winner;
 };
 
 // Do not edit below this line
